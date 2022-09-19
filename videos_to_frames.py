efcff828b17a6c957fb613acde1103ccd7c9acb1
nocomputer <wim@nocomputer.be>
2022-09-19 17:07:00 +0200

    Update videos_to_frames.py

--- videos_to_frames.py
+++ videos_to_frames.py
@@ -1,25 +1,30 @@
 import os
 import subprocess
 import shutil
-directory = "./"
+directory = "./take_2"
+
+print("START")
 
 def createFrames():
     global directory
     folders = os.listdir(directory)
+    
     for folder in folders:    
-        if os.path.isdir(folder):
+        full_folder_path = os.path.join(directory,folder)
+        if os.path.isdir(full_folder_path):
             #create a folder named frames
-            path = os.path.join(folder, "frames")
+            path = os.path.join(full_folder_path, "frames")
             if not os.path.exists(path):
                 os.mkdir(path)
-            movies = os.listdir(folder)
+            movies = os.listdir(full_folder_path)
+            
             for movie in movies:
-                file_path = os.path.join(folder,movie)
+                file_path = os.path.join(full_folder_path,movie)
                 if os.path.isfile(file_path):
                     
                     #remove spaces from names
                     newfilename = movie.replace(" ","")
-                    newpath = os.path.join(folder,newfilename)
+                    newpath = os.path.join(full_folder_path,newfilename)
                     if newfilename != movie:
                         os.rename(file_path, newpath)
 
@@ -36,37 +41,68 @@ def createFrames():
 def curateFrames():
     global directory
     folders = os.listdir(directory)
-    for folder in folders:    
-        if os.path.isdir(folder):
+    for folder in folders:
+        full_folder_path = os.path.join(directory,folder)
+        if os.path.isdir(full_folder_path):
             #create a folder named frames
-            target_folder = os.path.join(folder, "frames_curated")
+            target_folder = os.path.join(full_folder_path, "frames_curated")
             if not os.path.exists(target_folder):
                 os.mkdir(target_folder)
             
-            source_folder = os.path.join(folder, "frames")
-            print(source_folder, target_folder)
-
+            source_folder = os.path.join(full_folder_path, "frames")
+            print("CREATING ",source_folder, target_folder)
             source_frames = os.listdir(source_folder)
-           
             for count, element in enumerate(source_frames, 0): # Start counting from 1
                 if count % 8 == 0:
                     source_file = os.path.join(source_folder, element)
                     target_file = os.path.join(target_folder, element)
-                    shutil.move(source_file, target_file)
+                    shutil.copy(source_file, target_file)
 
 def cleanUPCurated():
     global directory
     folders = os.listdir(directory)
     for folder in folders:    
-        if os.path.isdir(folder):
-            target_folder = os.path.join(folder, "frames_curated")
+        full_folder_path = os.path.join(directory,folder)
+        if os.path.isdir(full_folder_path):
+            target_folder = os.path.join(full_folder_path, "frames_curated")
+            print("DELETE ",target_folder)
             if os.path.isdir(target_folder):
                 shutil.rmtree(target_folder)
 
 
-cleanUPCurated()
-curateFrames()
-
-
-
+def balance():
+    max_number = 1100
+    global directory
+    folders = os.listdir(directory)
+    for folder in folders:    
+        full_folder_path = os.path.join(directory,folder, "frames_curated")
+       
+        files = os.listdir(full_folder_path)
+        number_to_delete = len(files) - max_number
         
+        if number_to_delete>0:
+            print("DELETE FILES ", full_folder_path, len(files))
+            print("TOTAL TO DELETE ",number_to_delete)
+            every_n = round(len(files)/number_to_delete)  
+            print("DELETE EVERY ",every_n)
+           
+            ccount = 0
+            totalcount = 0
+
+            for file in files:
+                if ccount == every_n:
+                    ccount = 0
+                    totalcount = totalcount+1
+                    full_file_path = os.path.join(full_folder_path,file)
+                    print(full_file_path)
+                    os.remove(full_file_path)
+
+                ccount = ccount+1
+
+            print("TOTAL DELETED ",totalcount)
+            print("*****************************")
+
+#createFrames()
+#cleanUPCurated()
+#curateFrames()
+balance()
